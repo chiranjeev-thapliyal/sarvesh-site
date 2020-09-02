36d992fe535ad9021e75ad8b1ccc1b7c2ba1b796
Chiranjeev <49074304+chiranjeev-thapliyal@users.noreply.github.com>
2020-09-02 16:08:33 +0530

    added video support to modal

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var modalBoxContentIMG = document.querySelector('.modal__Box--content-active-img
 var images = document.querySelectorAll('.image-slider');
 var videos = document.querySelectorAll('.video-slider');
 
+var videosSource = ["https://www.youtube.com/embed/tgbNymZ7vqY","https://www.youtube.com/embed/tgbNymZ7vqY","https://www.youtube.com/embed/tgbNymZ7vqY","https://www.youtube.com/embed/tgbNymZ7vqY"];
+
 closeBtn.addEventListener("click", () => {
     modalBox.style.display = "none";
 })  
@@ -26,9 +28,9 @@ function nextVid(){
     var id = modalActiveImg.getAttribute('id');
     let currSlide = Number(id.substr(-1));
     if(currSlide < images.length-1){
-        modalBoxContentIMG.innerHTML = `<img class="modal-active-image" id="video-${currSlide+1}" src="${images[currSlide+1].src}">`;
+        modalBoxContentIMG.innerHTML = `<iframe class="modal-active-image" id="video-${currSlide+1}" src="${videosSource[currSlide+1]}"></iframe>`;
     }else{
-        modalBoxContentIMG.innerHTML = `<img class="modal-active-image" id="video-${0}" src="${images[0].src}">`;
+        modalBoxContentIMG.innerHTML = `<iframe class="modal-active-image" id="video-${0}" src="${videosSource[0]}"></iframe>`;
     }
 }
 
@@ -48,15 +50,19 @@ function prevVid(){
     var id = modalActiveImg.getAttribute('id');
     let currSlide = Number(id.substr(-1));
     if(currSlide > 0){
-        modalBoxContentIMG.innerHTML = `<img class="modal-active-image" id="video-${currSlide-1}" src="${images[currSlide-1].src}">`;
+        modalBoxContentIMG.innerHTML = `<iframe class="modal-active-image" id="video-${currSlide-1}" src="${videosSource[currSlide-1]}"></iframe>`;
     }else{
-        modalBoxContentIMG.innerHTML = `<img class="modal-active-image" id="video-${videos.length-1}" src="${images[0].src}">`;
+        modalBoxContentIMG.innerHTML = `<iframe class="modal-active-image" id="video-${videos.length-1}" src="${videosSource[videosSource.length-1]}"></iframe>`;
     }
 }
 
 function openModal(name){
     modalBox.style.display = "flex";
-    modalBoxContentIMG.innerHTML = `<img class="modal-active-image" id="${name.id}" src="${name.src}">`;
+    if(name.id.charAt(0) == 'p'){
+        modalBoxContentIMG.innerHTML = `<img class="modal-active-image" id="${name.id}" src="${name.src}">`;
+    }else{
+        modalBoxContentIMG.innerHTML = `<iframe class="modal-active-image" id="${name.id}" src="${videosSource[Number(name.id.substr(-1))]}"></iframe>`;
+    }
 }
 
 function changeModal(img){
